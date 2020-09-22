8e2f35f23ec3583bb008e851fa1b63a7e8fa8b5a
coreywelborn211 <70238975+coreywelborn211@users.noreply.github.com>
2020-09-22 15:17:03 -0500

    Add files via upload

---
+++ scripts.js
@@ -0,0 +1,45 @@
+// Function Stubs for Menu
+
+function optionA(){
+	document.getElementById("rightDiv").innerHTML = "Option A not implemented.";
+}
+
+function optionB(){
+	document.getElementById("rightDiv").innerHTML = "Option B not implemented.";
+}
+
+function optionC(){
+	document.getElementById("rightDiv").innerHTML = "Option C not implemented.";
+}
+
+function optionD(){
+	document.getElementById("rightDiv").innerHTML = "Option D not implemented.";
+}
+
+function optionE(){
+	document.getElementById("rightDiv").innerHTML = "Option E not implemented.";
+}
+
+function optionF(){
+	document.getElementById("rightDiv").innerHTML = "Option F not implemented.";
+}
+
+function optionG(){
+	document.getElementById("rightDiv").innerHTML = "Option G not implemented.";
+}
+
+function optionH(){
+	document.getElementById("rightDiv").innerHTML = "Option H not implemented.";
+}
+
+function optionI(){
+	document.getElementById("rightDiv").innerHTML = "Option I not implemented.";
+}
+
+function optionJ(){
+	document.getElementById("rightDiv").innerHTML = "Option J not implemented.";
+}
+
+function defaultBody(){
+	document.getElementById("rightDiv").innerHTML = "<h3>Default Text</h3>";
+}
